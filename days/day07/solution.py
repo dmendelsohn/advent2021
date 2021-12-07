5e272ab9803ac24e7591c23ee76f17c494451935
Daniel Mendelsohn <dmendelsohn@flatiron.com>
2021-12-07 00:27:00 -0500

    Day 7

--- days/day07/solution.py
+++ days/day07/solution.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from statistics import mean, median
 from typing import List
 
 INPUT_PATH = Path(__file__).parent / "input.txt"
@@ -10,14 +11,43 @@ def read_input(use_test_input: bool = False) -> str:
     return open(input_path).read().strip()
 
 
-def parse_input(use_test_input: bool = False) -> List[str]:
+def parse_input(use_test_input: bool = False) -> List[int]:
     raw_input = read_input(use_test_input)
-    return raw_input.split("\n")
+    return [int(elt) for elt in raw_input.split(",")]
+
+
+def get_cost(positions: List[int], target: int) -> int:
+    return sum(abs(pos - target) for pos in positions)
+
+
+def get_cost_2(positions: List[int], target: int) -> int:
+    cost = 0
+    for pos in positions:
+        n = abs(pos - target)
+        cost += n * (n + 1) // 2
+    return cost
 
 
 def part_1(use_test_input: bool = False) -> str:
-    pass
+    positions = parse_input(use_test_input)
+    print(positions)
+    target = round(median(positions))
+    print(target)
+    cost = get_cost(positions, target)
+    print(f"Moving to {target} would cost {cost}")
+    return f"{cost}"
 
 
 def part_2(use_test_input: bool = False) -> str:
-    pass
+    positions = parse_input(use_test_input)
+    # Try the number right below and right above the mean
+    lo_target = int(mean(positions))
+    hi_target = lo_target + 1
+    lo_cost = get_cost_2(positions, lo_target)
+    hi_cost = get_cost_2(positions, hi_target)
+    if lo_cost < hi_cost:
+        cost, target = lo_cost, lo_target
+    else:
+        cost, target = hi_cost, hi_target
+    print(f"Moving to {target} would cost {cost}")
+    return f"{cost}"
