22f7d57315ccaabef8018c9e99b2fc471faaccfc
Daniel Mendelsohn <dmendelsohn@flatiron.com>
2021-12-07 00:02:51 -0500

    Fix test rig and copy template into everywhere

--- days/day20/solution.py
+++ days/day20/solution.py
@@ -2,15 +2,22 @@ from pathlib import Path
 from typing import List
 
 INPUT_PATH = Path(__file__).parent / "input.txt"
+TEST_INPUT_PATH = Path(__file__).parent / "test_input.txt"
 
 
-def parse_input() -> List[str]:
-    return open(INPUT_PATH).read().strip().split("\n")
+def read_input(use_test_input: bool = False) -> str:
+    input_path = TEST_INPUT_PATH if use_test_input else INPUT_PATH
+    return open(input_path).read().strip()
 
 
-def part_1() -> str:
+def parse_input(use_test_input: bool = False) -> List[str]:
+    raw_input = read_input(use_test_input)
+    return raw_input.split("\n")
+
+
+def part_1(use_test_input: bool = False) -> str:
     pass
 
 
-def part_2() -> str:
+def part_2(use_test_input: bool = False) -> str:
     pass
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import importlib
 def main(day_num: int, part_num: int, is_test: bool) -> None:
     selected_solution_module = importlib.import_module(f"days.day{day_num:02d}.solution")
     selected_part_func = getattr(selected_solution_module, f"part_{part_num}")
-    if part_num < 7:  # Didn't support is_test yet
+    if day_num < 7:  # Didn't support is_test yet
         solution = selected_part_func()
         if is_test:
             raise ValueError("is_test=True not supported before Day 7")
